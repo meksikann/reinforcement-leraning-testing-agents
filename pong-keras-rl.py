7c296ad76b463ce176749148cca1258d0096e4c3
serhii <meksikann@gmail.com>
2019-05-02 09:57:52 +0300

    rl framwork

--- pong-keras-rl.py
+++ pong-keras-rl.py
@@ -16,27 +16,24 @@ from rl.memory import SequentialMemory
 from rl.core import Processor
 from rl.callbacks import FileLogger, ModelIntervalCheckpoint
 
-
 INPUT_SHAPE = (84, 84)
 WINDOW_LENGTH = 4
-
-
-print('Initialisation ---------------->>>>>>>>>>>>>>>>')
 
 
 class GamePreprocessor(Processor):
     def process_observation(self, observation):
         img = Image.fromarray(observation)
-        img = img.resize(INPUT_SHAPE).convert('L') # resize image and convert it to black/white
+        img = img.resize(INPUT_SHAPE).convert('L')  # resize image and convert it to black/white
         processed_observation = np.array(img)
-        return processed_observation.astype('uint8') # save in memory with type
+        return processed_observation.astype('uint8')  # save in memory with type
 
     def process_state(self, batch):
         processed_batch = batch.astype('float32') / 255
         return processed_batch
 
+
     def process_reward(self, reward):
-        return np.clip(reward, -1., 1.) # values smaler then -1 become -1 and values bigger then 1 become 1
+        return np.clip(reward, -1., 1.)  # values smaler then -1 become -1 and values bigger then 1 become 1
 
 
 # get command arguments
@@ -53,7 +50,7 @@ env.seed(123)
 actions_number = env.action_space.n
 
 # build model
-input_shape = (WINDOW_LENGTH, ) + INPUT_SHAPE
+input_shape = (WINDOW_LENGTH,) + INPUT_SHAPE
 model = Sequential()
 
 # check image ordering if 'tensorflow' or 'theano' and add first layer
@@ -80,7 +77,63 @@ model.add(Activation('linear'))
 
 model.summary()
 
+print('Initialisation ---------------->>>>>>>>>>>>>>>>')
+
 # init the Agent
 memory = SequentialMemory(limit=1000000, window_length=WINDOW_LENGTH)
 atari_agent = GamePreprocessor()
 
+# choose policy (epsilon-greedy)
+policy = LinearAnnealedPolicy(
+    EpsGreedyQPolicy(),
+    attr='eps',
+    value_max=1.,  # eps max
+    value_min=0.1,  # eps min
+    value_test=0.05,  # eps value during testing
+    nb_steps=1000000  # eps steps
+)
+
+# init RL agent, with model, gamma and cetra
+dqn = DQNAgent(
+    model=model,
+    nb_actions=actions_number,
+    policy=policy,
+    memory=memory,
+    processor=atari_agent,
+    nb_steps_warmup=50000,
+    gamma=.99,
+    target_model_update=10000,
+    train_interval=4,
+    delta_clip=1.
+)
+
+# compile DQN agent
+dqn.compile(
+    Adam(lr=.00025),
+    metrics=['mae']
+)
+
+# run agents mode
+if arguments.mode == 'train':
+    weights_file = 'dqn_{}_weights.h5f'.format(arguments.env_name)
+    checkpoint_filename = 'dqn_' + arguments.env_name + '_weights_{step}.h5f'
+    logs = 'dqn_{}_log.json'.format(arguments.env_name)
+    callbacks = [ModelIntervalCheckpoint(checkpoint_filename, interval=250000)]
+    callbacks += [FileLogger(logs, interval=100)]
+
+    # fit the aqn
+    dqn.fit(env, callbacks=callbacks, nb_steps=1750000, log_interval=10000)
+
+    # save weights
+    dqn.save_weights(weights_file, overwrite=True)
+
+    #  evaluate algorithm
+    dqn.test(env, nb_episodes=10, visualize=True)
+
+# test agent
+elif args.mode == 'test':
+    weights_filename = 'dqn_{}_weights.h5f'.format(args.env_name)
+    if args.weights:
+        weights_filename = args.weights
+    dqn.load_weights(weights_filename)
+    dqn.test(env, nb_episodes=10, visualize=True)
--- test.py
+++ test.py
@@ -77,7 +77,7 @@ def start_pong():
             model.fit(x=np.vstack(x_train),
                       y=np.vstack(y_train),
                       verbose=0,
-                      callbacks=[tbCallBack],
+                      # callbacks=[tbCallBack],
                       sample_weight=discount_rewards(rewards, gamma))
 
             running_reward = reward_sum if running_reward is None else running_reward * 0.99 + reward_sum * 0.01
